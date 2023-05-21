103d0833c036f6d764d6a2c941044f5a99878295
ReiraEndo <reira.endo@gmail.com>
2023-05-21 11:18:22 +0000

    Html and views prepare

--- projectManagement/urls.py
+++ projectManagement/urls.py
@@ -5,19 +5,18 @@ urlpatterns = [
     #アカウント画面系URL
     #ログイン画面
     #path('login/', views.login, name='login'),
-    ]
-'''#アカウント作成画面
+    #アカウント作成画面
     path('account/create/', views.account_create, name='account_create'),
     #アカウント作成完了画面
-    path('account/create/complete/', views.account_create_complete, name='account_create_complete'),
+    #path('account/create/complete/', views.account_create_complete, name='account_create_complete'),
     #アカウント再設定リンク画面
-    path('account/settings/e-mail-edit-link/', views.email_edit, name='email_edit'),
+    #path('account/settings/e-mail-edit-link/', views.email_edit, name='email_edit'),
     #アカウント再設定完了画面
-    path('account/settings/e-mail-edit/complete/', views.email_edit_complete, name='email_edit_complete'),
+    #path('account/settings/e-mail-edit/complete/', views.email_edit_complete, name='email_edit_complete'),
     #パスワードリセット画面
-    path('account/password_reset/', views.password_reset, name='password_reset'),
+    #path('account/password_reset/', views.password_reset, name='password_reset'),
     #パスワード再設定完了画面
-    path('account/password_reset/complete/', views.password_reset_complete, name='password_reset_complete'),
+    #path('account/password_reset/complete/', views.password_reset_complete, name='password_reset_complete'),
    
     
     #プロジェクト系
@@ -51,4 +50,5 @@ urlpatterns = [
     path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/tasks/create/', views.create_task, name='create_task'),
     #タスク詳細編集画面
     path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/tasks/<int:task_id>/edit/', views.edit_task, name='edit_task'),
-    '''
+    ]
+
--- projectManagement/views.py
+++ projectManagement/views.py
@@ -1,42 +1,53 @@
 from django.shortcuts import render
 
-#Create your views here.
-#アカウント作成
-#account_create
+# アカウント作成
+def account_create(request):
+    return render(request, 'account_create.html')
 
-#プロジェクト一覧
-#project_list
+# プロジェクト一覧
+def project_list(request):
+    return render(request, 'project_list.html')
 
-#プロジェクト作成
-#create_project
+# プロジェクト作成
+def create_project(request):
+    return render(request, 'create_project.html')
 
-#プロジェクト編集
-#edit_project
+# プロジェクト編集
+def edit_project(request):
+    return render(request, 'edit_project.html')
 
-#プロジェクト詳細
-#project_detail
+# プロジェクト詳細
+def project_detail(request):
+    return render(request, 'project_detail.html')
 
-#フェーズ詳細
-#phase_detail
+# フェーズ詳細
+def phase_detail(request):
+    return render(request, 'phase_detail.html')
 
-#フェーズ作成
-#create_phase
+# フェーズ作成
+def create_phase(request):
+    return render(request, 'create_phase.html')
 
-#フェーズ編集
-#edit_phase
+# フェーズ編集
+def edit_phase(request):
+    return render(request, 'edit_phase.html')
 
-#ユニット作成
-#create_unit
+# ユニット作成
+def create_unit(request):
+    return render(request, 'create_unit.html')
 
-#ユニット編集
-#edit_unit
+# ユニット編集
+def edit_unit(request):
+    return render(request, 'edit_unit.html')
 
-#タスク詳細
-#task_detail
+# タスク詳細
+def task_detail(request):
+    return render(request, 'task_detail.html')
 
-#タスク作成
-#create_task
+# タスク作成
+def create_task(request):
+    return render(request, 'create_task.html')
 
-#タスク編集
-#edit_task
-
+# タスク編集
+def edit_task(request):
+    return render(request, 'edit_task.html')
