62a88b7e309a402c4ab759293b5d38c5dac79ed1
ReiraEndo <reira.endo@gmail.com>
2023-05-21 07:13:19 +0000

    urls and models commit

--- projectManagement/models.py
+++ projectManagement/models.py
@@ -1,3 +1,123 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    members = models.ManyToManyField(
+        AbstractUser,
+        through='ProjectMember',
+        related_name='projects',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class ProjectMember(models.Model):
+    PROJECT_ROLE_CHOICES = [
+        ('manager', 'Manager'),
+        ('worker', 'Worker'),
+        ('stakeholder', 'Stakeholder'),
+    ]
+    user = models.ForeignKey(
+        AbstractUser,
+        on_delete=models.CASCADE,
+        related_name='project_memberships',
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='memberships',
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=PROJECT_ROLE_CHOICES,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'project')
+
+
+class Phase(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='phases',
+    )
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-start_date']
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    phase = models.ForeignKey(
+        Phase,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    assigned_users = models.ManyToManyField(
+        AbstractUser,
+        through='TaskAssignment',
+        related_name='task_assignments',
+    )
+
+    def is_completed(self):
+        if not hasattr(self, '_is_completed'):
+            self._is_completed = all(
+                task_assignment.is_completed
+                for task_assignment in self.taskassignment_set.all()
+            )
+        return self._is_completed
+
+
+class TaskAssignment(models.Model):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        AbstractUser,
+        on_delete=models.CASCADE,
+        related_name='task_assignments',
+    )
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('task', 'user',)
+
+    def complete(self):
+        self.is_completed = True
+        self.save()
+        if self.task.is_completed():
+            # タスクが完了した場合の処理
+            pass
+
--- projectManagement/urls.py
+++ projectManagement/urls.py
@@ -1,5 +1,24 @@
 from django.urls import path
- 
+from . import views
+
 urlpatterns = [
-  
+    path('login/', views.login, name='login'),
+    path('account/create/', views.account_create, name='account_create'),
+    path('projects/', views.project_list, name='project_list'),
+    path('account/password_reset/', views.password_reset, name='password_reset'),
+    path('projects/<int:project_id>/manager', views.project_detail_manager, name='project_detail_manager'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/worker/', views.phase_detail_worker, name='phase_detail_worker'),
+    path('projects/create/', views.create_project, name='create_project'),
+    path('account/create/complete/', views.account_create_complete, name='account_create_complete'),
+    path('account/password_reset/complete/', views.password_reset_complete, name='password_reset_complete'),
+    path('projects/<int:project_id>/edit/', views.edit_project, name='edit_project'),
+    path('projects/<int:project_id>/phases/create/', views.create_phase, name='create_phase'),
+    path('account/settings/e-mail-edit-link/', views.email_edit, name='email_edit'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/edit/', views.edit_unit, name='edit_unit'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/tasks/create/', views.create_task, name='create_task'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/units/create/', views.create_unit, name='create_unit'),
+    path('account/settings/e-mail-edit/complete/', views.email_edit_complete, name='email_edit_complete'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/units/<int:unit_id>/tasks/<int:task_id>/edit/', views.edit_task, name='edit_task'),
+    path('projects/<int:project_id>/phases/<int:phase_id>/edit/', views.edit_phase, name='edit_phase'),
 ]
