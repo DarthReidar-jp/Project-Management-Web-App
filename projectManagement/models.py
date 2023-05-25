0e0f6fdb38dbe16dc7969b87819a6931fa0146f6
ReiraEndo <reira.endo@gmail.com>
2023-05-25 14:23:50 +0000

    models.py
    承認系統を整えた

--- projectManagement/models.py
+++ projectManagement/models.py
@@ -6,9 +6,9 @@ User = settings.AUTH_USER_MODEL
 class Project(models.Model):
     project_name = models.CharField(max_length=255)
     project_description = models.TextField()
-    project_kind = models.CharField(max_length=255)#種類を準備する
+    project_kind = models.CharField(max_length=255)
     responsible = models.ForeignKey(User, on_delete=models.CASCADE)
-    priority = models.IntegerField()#優先度を準備する
+    priority = models.IntegerField()
     invitation_id = models.CharField(max_length=255)
     dead_line = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,20 +16,24 @@ class Project(models.Model):
     is_completed_project = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['-created_at']  # 作成日時の降順でソート
+        ordering = ['-created_at']
     
     def __str__(self):
         return self.project_name
     
-    #ここら辺のコードがうまく働きません
     def save(self, *args, **kwargs):
-        if not self.pk or not self.phases.exists():
-            self.is_completed_project = False
-        elif self.phases.exists() and self.phases.filter(is_completed_phase=False).exists():
-            self.is_completed_project = False
-        else:
-            self.is_completed_project = True
         super().save(*args, **kwargs)
+
+        phases = self.phases.all()
+        if not self.pk or not phases.exists():
+            self.is_completed_project = False
+        elif phases.exists() and not phases.filter(is_completed_phase=False).exists():
+            self.is_completed_project = True
+        else:
+            self.is_completed_project = False
+
+        self.__class__.objects.filter(pk=self.pk).update(is_completed_project=self.is_completed_project)
+
 
 
 class ProjectMember(models.Model):
@@ -56,7 +60,7 @@ class ProjectMember(models.Model):
 class Phase(models.Model):
     phase_name = models.CharField(max_length=255)
     phase_description = models.TextField()
-    project = models.ForeignKey(Project,on_delete=models.CASCADE)  # フェーズが所属するプロジェクト
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="phases")
     start_day = models.DateField()
     dead_line = models.DateField()
     is_completed_phase = models.BooleanField(default=False)
@@ -64,25 +68,29 @@ class Phase(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-start_day']  # 開始日時の降順でソート
+        ordering = ['-start_day']
+
     def __str__(self):
         return str(self.phase_name)
-    def __str__(self):
-        return self.phase_name
+
     def save(self, *args, **kwargs):
-        if not self.pk or not self.units.exists():
-            self.is_completed_phase = False
-        elif self.units.exists() and self.units.filter(is_completed_unit=False).exists():
-            self.is_completed_phase = False
-        else:
-            self.is_completed_phase = True
         super().save(*args, **kwargs)
+
+        units = self.units.all()
+        if not self.pk or not units.exists():
+            self.is_completed_phase = False
+        elif units.exists() and not units.filter(is_completed_unit=False).exists():
+            self.is_completed_phase = True
+        else:
+            self.is_completed_phase = False
+
+        self.__class__.objects.filter(pk=self.pk).update(is_completed_phase=self.is_completed_phase)
 
 
 class Unit(models.Model):
     unit_name = models.CharField(max_length=255)
     unit_description = models.TextField()
-    phase = models.ForeignKey(Phase, on_delete=models.CASCADE)
+    phase = models.ForeignKey(Phase, on_delete=models.CASCADE, related_name="units")
     start_day = models.DateField()
     dead_line = models.DateField()
     is_completed_unit = models.BooleanField(default=False)
@@ -91,20 +99,26 @@ class Unit(models.Model):
 
     def __str__(self):
         return self.unit_name
+
     def save(self, *args, **kwargs):
-        if not self.pk or not self.tasks.exists():
-            self.is_completed_unit = False
-        elif self.tasks.exists() and self.tasks.filter(is_completed_task=False).exists():
-            self.is_completed_unit = False
-        else:
-            self.is_completed_unit = True
         super().save(*args, **kwargs)
+
+        tasks = self.tasks.all()
+        if not self.pk or not tasks.exists():
+            self.is_completed_unit = False
+        elif tasks.exists() and not tasks.filter(is_completed_task=False).exists():
+            self.is_completed_unit = True
+        else:
+            self.is_completed_unit = False
+
+        self.__class__.objects.filter(pk=self.pk).update(is_completed_unit=self.is_completed_unit)
+
 
 
 class Task(models.Model):
     task_name = models.CharField(max_length=255)
     task_description = models.TextField()
-    unit = models.ForeignKey(Unit,on_delete=models.CASCADE,) 
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name="tasks") 
     start_day = models.DateField()
     dead_line = models.DateField()
     is_completed_task = models.BooleanField(default=False)
@@ -113,22 +127,32 @@ class Task(models.Model):
 
     def __str__(self):
         return self.task_name
+
     def save(self, *args, **kwargs):
-        if not self.pk or not self.taskassignments.exists():
+        super().save(*args, **kwargs)  # 保存処理を先に実行
+
+        assignments = self.assignments.all()
+        if not self.pk or not assignments.exists():
             self.is_completed_task = False
-        elif self.taskassignments.exists() and self.taskassignment.filter(is_completed_taskmember=False).exists():
-            self.is_completed_task = False
-        else:
+        elif assignments.exists() and not assignments.filter(is_completed_member=False).exists():
             self.is_completed_task = True
-        super().save(*args, **kwargs)
+        else:
+            self.is_completed_task = False
+
+        # `is_completed_task`のみを更新
+        self.__class__.objects.filter(pk=self.pk).update(is_completed_task=self.is_completed_task)
+
 
 class TaskAssignment(models.Model):
     """タスク割り当てモデル"""
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE,related_name="assignments")
     project_member = models.ForeignKey(ProjectMember, on_delete=models.CASCADE)
-    is_completed_taskmember= models.BooleanField(default=False)
+    is_completed_member= models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.task} - {self.project_member}"
+    
+
+
