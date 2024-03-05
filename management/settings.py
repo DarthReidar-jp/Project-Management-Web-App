f8aa656fa49bfd2bef880132a38140a0a0fec6bc
ReiraEndo <reira.endo@gmail.com>
2024-03-05 12:31:18 +0000

    imagi static

--- management/settings.py
+++ management/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'allauth.account.middleware.AccountMiddleware', 
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
